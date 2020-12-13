8550d2b9402cc5ea366a4a21108d71a0f3fb2fe6
Lakshya Kumar <kamrakumar275@gmail.com>
2020-12-13 15:24:22 +0530

    initial commit

---
+++ script/script.js
@@ -0,0 +1,24 @@
+console.log('temp');
+
+function closer(btnId, PlaceId) {
+    let btn = document.getElementById(btnId)
+    let place = document.getElementById(PlaceId)
+
+    btn.addEventListener('click', () => {
+        if (place.style.display != 'none') {
+            place.style.display = 'none'
+            
+        }
+        else {
+            place.style.display = 'block'
+        }
+    })
+}
+closer('btn1', 'btn-close1');
+closer('btn2', 'btn-close2');
+closer('btn3', 'btn-close3');
+closer('btn4', 'btn-close4');
+closer('btn5', 'btn-close5');
+closer('btn6', 'btn-close6');
+closer('btn7', 'btn-close7');
+closer('btn8', 'btn-close8');
