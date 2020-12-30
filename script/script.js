ce119843604359946a5d33ef15363cec03df72e8
Lakshya Kumar <kamrakumar275@gmail.com>
2020-12-30 20:25:57 +0530

    initial commit

--- script/script.js
+++ script/script.js
@@ -24,3 +24,16 @@ closer('btn7', 'btn-close7');
 closer('btn8', 'btn-close8');
 closer('btn9', 'btn-close9');
 closer('btn10', 'btn-close10');
+closer('btn11', 'btn-close11');
+// closer('btn12', 'btn-close12');
+// closer('btn13', 'btn-close13');
+// closer('btn14', 'btn-close14');
+// closer('btn15', 'btn-close15');
+// closer('btn16', 'btn-close16');
+// closer('btn17', 'btn-close17');
+// closer('btn18', 'btn-close18');
+// closer('btn19', 'btn-close19');
+// closer('btn20', 'btn-close20');
+// closer('btn21', 'btn-close21');
+// closer('btn22', 'btn-close22');
+
