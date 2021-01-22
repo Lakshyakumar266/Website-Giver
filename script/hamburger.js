14419f2f3c144063ec288604fa8960c08698664a
Lakshya Kumar <kamrakumar275@gmail.com>
2021-01-22 16:31:26 +0530

    initial commit

---
+++ script/hamburger.js
@@ -0,0 +1,10 @@
+
+function hamburgerOpean() {
+    console.log('hamburger');
+
+    var menuHight = document.getElementById('navul');
+    var navigation = document.getElementById('navigation');
+
+    menuHight.classList.toggle('nav-list');
+    navigation.classList.toggle('burger');
+}
