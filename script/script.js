fc3b33c4a25260fcd42dd486d86bacae5ff73f1b
Lakshya Kumar <kamrakumar275@gmail.com>
2021-01-26 19:38:41 +0530

    initial commit

--- script/script.js
+++ script/script.js
@@ -1,21 +1,28 @@
 console.log('temp');
 
-function closer(btnId, PlaceId) {
+function closer(btnId, PlaceId, arrow) {
     let btn = document.getElementById(btnId)
     let place = document.getElementById(PlaceId)
+    let Arrow = document.getElementsByClassName(arrow)
 
-    btn.addEventListener('click', () => {
-        if (place.style.display != 'none') {
-            place.style.display = 'none';
-        }
-        else {
-            place.style.display = 'block';
-        }
-    })
+    for (let i = 0; i < Arrow.length; i++) {
+        const element = Arrow[i];
+
+        btn.addEventListener('click', () => {
+            if (place.style.display != 'none') {
+                place.style.display = 'none';
+                element.classList.remove('anim');
+            }
+            else {
+                place.style.display = 'block';
+                element.classList.add('anim');
+            }
+        })
+    }
 }
 
 let buttons_div = document.getElementsByClassName('data-hande');
 for (let x = 0; x < buttons_div.length; x++) {
     const element = buttons_div[x];
-    closer(`btn${x + 1}`, `btn-close${x + 1}`);
+    closer(`btn${x + 1}`, `btn-close${x + 1}`, `${x + 1}`);
 }
