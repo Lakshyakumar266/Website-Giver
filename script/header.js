e46f9ddb9a985e64a22cc4e1df1200abdc3937ad
Lakshya Kumar <kamrakumar275@gmail.com>
2021-01-30 20:24:25 +0530

    initial commit

--- script/header.js
+++ script/header.js
@@ -18,3 +18,20 @@ document.querySelector('nav').innerHTML = `
         </ul>
     </div>
 `;
+
+// footer
+document.querySelector('footer').innerHTML = `
+    Copyright &copy; all Rights reserved &#8205; <span id="year-holder"></span>
+    <span class="space">&#8205; &#8205; | &#8205; &#8205;</span>
+    <!-- Images Copyright -->
+    <div>
+        Vector Images From -<a href="https://www.freepik.com/vectors" target="_blank"> freepik.com</a>
+    </div>
+    <br>
+    <div>
+        <span class="and"> &#8205; &#8205; With &#8205; &#8205; </span>
+        <a href="https://www.freepnglogos.com" target="_blank">freepnglogos.co</a>
+        <span class="and"> &#8205; &#8205; And &#8205; &#8205; </span>
+        <a href="https://secure.logomaker.com/" target="_blank">logomaker.com</a>
+    </div>
+`;
